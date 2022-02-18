d30f768df505c9af7c4e5b012ea0b50e0e0b0bd8
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-02-18 13:37:35 +0530

    Add files via upload

---
+++ Java Programs For Github/Basic Programs/ASCII number.java
@@ -0,0 +1,9 @@
+// Ascii Program to find out ASCII Character for that number
+
+class Ascii 
+{
+    public static void main (String[] args) {
+        int num=99;
+        System.out.println("Alphabet is:" +(char)num );
+    }
+}
