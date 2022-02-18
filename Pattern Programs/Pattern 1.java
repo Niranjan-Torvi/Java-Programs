347ae6e08d0049f6d0616836a2b7261f99a980f8
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-02-18 12:16:44 +0530

    Update Pattern 1.java

--- Pattern Programs/Pattern 1.java
+++ Pattern Programs/Pattern 1.java
@@ -10,7 +10,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 1; i <= n; i++)
         {
