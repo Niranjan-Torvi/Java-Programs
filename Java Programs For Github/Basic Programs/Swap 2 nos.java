f1cc2942781b56e3593021962d5c91b6b3aa03c2
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-02-18 13:35:43 +0530

    Add files via upload

---
+++ Java Programs For Github/Basic Programs/Swap 2 nos.java
@@ -0,0 +1,16 @@
+// Swap Two Nos
+class Swap
+{
+    public static void main (String[] args) {
+        int a=3;
+        int b=5;
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("The value of a" +a );
+        System.out.println("The value of b" +b );
+        
+        
+    }
+}
