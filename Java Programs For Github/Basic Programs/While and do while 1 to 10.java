82ca640f8369f187d07cde24b08805cf1fc29843
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-02-18 13:32:25 +0530

    Add files via upload

---
+++ Java Programs For Github/Basic Programs/While and do while 1 to 10.java
@@ -0,0 +1,27 @@
+// While and Do while 1 to 10 Print
+public class MyClass {
+    public static void main(String args[]) {
+     int i = 1;
+     do
+     {
+         System.out.println(i);
+         i++;
+     }
+     while(i <= 10);
+     
+    }
+}
+While Loop 1 to 10
+public class MyClass {
+    public static void main(String args[]) {
+     int i = 1;
+     while(i <= 10)
+     {
+         System.out.println(i);
+         i++;
+     }
+     
+     
+    }
+}
+
