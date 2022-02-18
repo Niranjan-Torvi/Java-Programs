0e5198816b735aeadd50492a9ade1636b0ce2604
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-02-18 13:28:54 +0530

    Add files via upload

---
+++ Java Programs For Github/Basic Programs/Leap Year.java
@@ -0,0 +1,14 @@
+// Leap Year
+class Leap
+{
+    public static void main (String[] args) {
+        /* code */
+    
+        int num=2000; 
+        if(num % 4 == 0)
+        {
+            System.out.println("It is leap year");
+        }
+        System.out.println("It is not a leap year");
+    }    
+}
