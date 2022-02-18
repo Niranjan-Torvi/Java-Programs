2f7718370d95ec9e759bae5faacb626feef8e076
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-02-18 12:53:31 +0530

    Add files via upload

---
+++ Java Programs For Github/Basic Programs/Palindrome string.java
@@ -0,0 +1,22 @@
+// Palindrome string
+
+class Palindromestr
+{
+    public static void main (String[] args) {
+        
+    
+       String str="Niranjan";
+       String rev="";
+       for(int i=str.length()-1;i>=0;i--)
+       {
+            rev=rev + str.charAt(i);
+       }
+       if (str.equals (rev))
+       {
+           System.out.println("It is palindrome");
+       }
+       else{
+           System.out.println("It is not palindrome");
+       }  
+    }   
+}
