9293583c848f17b82b5fb11a7b13213359068e89
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-03-03 18:34:10 +0530

    Add files via upload

---
+++ Java Programs For Github/Basic Programs/Swap two nos without using third variable.java
@@ -0,0 +1,24 @@
+// Swap two nos without using third variable
+
+public class MyClass {
+    
+       
+      
+       public static void main(String args[]) {
+          int num1 = 5;
+          int num2 = 6;
+          num1 = num1 + num2;
+          num2 = num1 - num2;
+          num1 = num1 - num2;
+          
+          System.out.println(num1);
+          System.out.println(num2);
+         
+        
+           
+       
+       
+     
+    
+    }
+}
