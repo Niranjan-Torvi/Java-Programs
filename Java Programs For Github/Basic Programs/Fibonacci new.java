62fe3021cae14725e2eca2b9601257f2ff893455
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-03-04 09:39:10 +0530

    Add files via upload

---
+++ Java Programs For Github/Basic Programs/Fibonacci new.java
@@ -0,0 +1,34 @@
+// Fibonacci 
+
+public class MyClass {
+    
+       
+      
+       public static void main(String args[]) {
+           int n = 9;
+           int a = 0;
+           int b = 1;
+           System.out.println(a);
+           System.out.println(b);
+           for(int i = 1; i <= n; i++)
+           {
+                int c = a + b;
+                System.out.println(c);
+                a = b;
+                b = c;
+          
+           
+               
+           }
+          
+           
+           
+          
+          
+        }
+           
+       
+       
+     
+}
+    
