c6d68e78dbd159f9ef91a4651d92a2d529a36324
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-03-04 10:00:15 +0530

    Add files via upload

---
+++ Arrays and Functions/Reverse array.java
@@ -0,0 +1,22 @@
+// Reverse an array
+
+
+public class MyClass {
+    
+       
+      
+       public static void main(String args[]) {
+           int arr[] = {21,12,45,34};
+           
+           for(int i = arr.length - 1; i >= 0; i--)
+           {
+               System.out.println(arr[i]);
+           
+           }
+        }
+           
+       
+       
+     
+}
+    
