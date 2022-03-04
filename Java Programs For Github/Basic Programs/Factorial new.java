29464307b6cba757e502f3537f558670320d1a32
Niranjan_T_git <76902611+Niranjan-Torvi@users.noreply.github.com>
2022-03-04 09:32:20 +0530

    Add files via upload

---
+++ Java Programs For Github/Basic Programs/Factorial new.java
@@ -0,0 +1,24 @@
+// Factorial Program
+
+public class MyClass {
+    
+       
+      
+       public static void main(String args[]) {
+           int n = 5;
+           int fact = 1;
+           
+           for(int i = 1; i <= n; i++)
+           {
+              fact = fact * i;
+           }
+           System.out.println(fact);
+          
+          
+        }
+           
+       
+       
+     
+}
+    
